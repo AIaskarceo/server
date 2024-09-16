d573d0883934315eb9efd4f671c763c5367be481
AIaskarceo <askarmani882@gmail.com>
2024-09-16 15:06:15 +0530

    added

--- server.js
+++ server.js
@@ -7,9 +7,17 @@ const app = express();
 // Define the port number
 const PORT = 3000;
 
+// Middleware to get client IP address
+app.use((req, res, next) => {
+    const userIP = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    console.log(`Access from IP: ${userIP}`);
+    req.userIP = userIP;  // Storing the IP in the request object for later use
+    next();
+});
+
 // Defining a simple route
 app.get('/', (req, res) => {
-    res.send('Hello, this is a simple server!');
+    res.send(`Hello, this is a simple server! Your IP address is: ${req.userIP}`);
 });
 
 // Starting the server
