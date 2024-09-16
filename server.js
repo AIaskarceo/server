cfd60b9bf2f8ba12a8afb9bdf4bb6d97fc05c766
AIaskarceo <askarmani882@gmail.com>
2024-09-16 15:18:40 +0530

    added

--- server.js
+++ server.js
@@ -7,9 +7,18 @@ const app = express();
 // Define the port number
 const PORT = 3000;
 
+// Array to store all user IP addresses
+let userIPs = [];
+
 // Middleware to get client IP address
 app.use((req, res, next) => {
     const userIP = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+
+    // If the IP is not already in the list, add it
+    if (!userIPs.includes(userIP)) {
+        userIPs.push(userIP);
+    }
+
     console.log(`Access from IP: ${userIP}`);
     req.userIP = userIP;  // Storing the IP in the request object for later use
     next();
@@ -17,7 +26,14 @@ app.use((req, res, next) => {
 
 // Defining a simple route
 app.get('/', (req, res) => {
-    res.send(`Hello, this is a simple server! Your IP address is: ${req.userIP}`);
+    // Display all users' IP addresses on the webpage
+    const allIPs = userIPs.map(ip => `<li>${ip}</li>`).join('');
+    res.send(`
+        <h1>Hello, this is a simple server!</h1>
+        <p>Your IP address is: ${req.userIP}</p>
+        <h2>All Users' IP Addresses:</h2>
+        <ul>${allIPs}</ul>
+    `);
 });
 
 // Starting the server
